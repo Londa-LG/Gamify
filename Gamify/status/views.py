e071f16d6aa9835ac1dd905d0fffc2f7e80d4224
Londa-LG <66629166+Londa-LG@users.noreply.github.com>
2020-08-12 17:27:55 +0200

    Charts
    Now I've figured out how to make charts display.

--- Gamify/status/views.py
+++ Gamify/status/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from User.models import Status
+from django.contrib.auth.models import User
 
 
 def PlayerView(request):
-    return render(request,'status/Status.html')
+    user1 = User.objects.get(username='Londa')
+    stats = Status.objects.get(user=user1)
+    content = {'status': stats }
+    return render(request,'status/Status.html',content)
